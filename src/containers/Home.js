f47fc5ad3e61e463a5989226ca9b212858c9c00c
Alejandro Villarreal <avillarreal01@gmail.com>
2018-01-17 18:52:46 -0500

    add sign in pages

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Reader Pro</h1>
+          <p>An RSS reader that works.</p>
+        </div>
+      </div>
+    );
+  }
+}
