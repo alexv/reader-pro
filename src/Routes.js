d8978a842a80f957b3aa9c82fb190ecbe8996361
Alejandro Villarreal <avillarreal01@gmail.com>
2018-01-18 12:01:23 -0500

    add signup sheet

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 
 import Home from './containers/Home';
 import Login from './containers/Login';
+import Signup from './containers/Signup';
 import NotFound from './containers/NotFound';
 import AppliedRoute from './components/AppliedRoute';
 
@@ -11,6 +12,7 @@ const Routes = ({ childProps }) => (
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     {/* Finally, catch all unmatched routes */}
     <Route component={NotFound} />
   </Switch>
