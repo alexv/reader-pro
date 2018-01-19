8ae0c8f3c1344e42c4fce969c8b1c44cade435e2
Alejandro Villarreal <avillarreal01@gmail.com>
2018-01-19 17:23:55 -0500

    add config

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+  cognito: {
+    USER_POOL_ID: 'us-east-2_496YOvDZl',
+    APP_CLIENT_ID: '3uo5bs812nmhfkbuoprkgbgmsi',
+    REGION: 'us-east-2',
+    IDENTITY_POOL_ID: 'us-east-2:d9a66f48-39b9-4cbb-a236-0f56a4e0ab99',
+  },
+  apiGateway: {
+    URL: 'https://rey6dybbva.execute-api.us-east-2.amazonaws.com/prod',
+    REGION: 'us-east-2',
+  },
+};
